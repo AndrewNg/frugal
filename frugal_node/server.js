b5ca8a94394a00644fee49a461ff921671d3777c
AndrewNg <andrew@chesscademy.com>
2014-06-21 12:21:35 -0400

    venmo payments are working. awwww yeah

--- frugal_node/server.js
+++ frugal_node/server.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var exec = require('exec');
 var app = express();
+
+var pay = function(accessToken, amount, note){
+  var curlString = 'curl https://api.venmo.com/v1/payments -d access_token=' + accessToken + ' -d email="francis@chesscademy.com" -d amount=' + amount + ' -d note="' + note + '"';
+  var child = exec(curlString, function(error, stdout, stderr){
+    console.log(curlString);
+    if(error){
+      console.log('Error: ' + error);
+    }
+  });
+};
 
 app.get('/', function(req, res){
   var accessToken = (req.get('host') + req.originalUrl).substring(29);
   console.log(accessToken);
+
+  pay(accessToken, 1, "What's up Francis. I hope this works.");
   res.send("hello world");
 });
 
